d14207345eb7605f08c0657e56beee52f3a4ca3c
nguyenanhquan13 <anhquan130204@gmail.com>
2024-08-07 20:32:33 +0700

    mo hinh mvc

---
+++ src/app/controllers/SiteController.js
@@ -0,0 +1,16 @@
+
+
+class SiteController {
+
+    //[Get] /
+    index(req, res) {
+        res.render('home');
+    }
+
+    //[Get] /search
+    search(req, res){
+        res.render('search');
+    }
+}
+
+module.exports = new SiteController;
---
+++ src/index.js
@@ -0,0 +1,39 @@
+const path = require("path");
+const express = require("express");
+const morgan = require("morgan");
+const { engine } = require("express-handlebars");
+const app = express();
+const port = 3000;
+
+const route = require('./routes');
+
+//Static file 
+app.use(express.static(path.join(__dirname, "public")));
+
+//Middleware xử lý dữ liệu từ form
+app.use(express.urlencoded({
+  extended: true
+}));
+app.use(express.json({ limit: 10 }))
+
+// HTTP logger
+app.use(morgan("combined"));
+
+// Template engine
+app.engine("hbs", engine({
+  extname: '.hbs'
+}));
+app.set("view engine", "hbs");
+app.set("views", path.join(__dirname, "resources/views"));
+
+//Route init
+route(app);
+
+//localhost --- hosting
+
+// Action --> Dispatcher --> functionHandler
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
